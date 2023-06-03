91dcc5bdca7d7b1ecf669d7228bfdef340b1b6c1
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-03 10:52:59 +0500

    product controller added

--- Solution1/IndexMarket.Api/Controllers/ProductController.cs
+++ Solution1/IndexMarket.Api/Controllers/ProductController.cs
@@ -1,6 +1,4 @@
-﻿using IndexMarket.Application.DataTransferObject;
-using IndexMarket.Application.Services;
-using Microsoft.AspNetCore.Http;
+﻿using IndexMarket.Application.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IndexMarket.Api.Controllers;
@@ -9,18 +7,12 @@ namespace IndexMarket.Api.Controllers;
 public class ProductController : ControllerBase
 {
     private readonly IProductServices productServices;
-    public ProductController(IProductServices productServices)
+    public ProductController(
+        IProductServices productServices)
     {
         this.productServices = productServices;
     }
 
-    [HttpPost]
-    public async ValueTask<ActionResult<ProductDto>> PostProductAsync(
-        ProductForCreationDto productForCreation)
-    {
-        var createProduct = await this.productServices
-            .CreateProductAsync(productForCreation);
 
-        return Created("", createProduct);
-    }
+
 }
