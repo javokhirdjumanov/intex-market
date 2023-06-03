c0add63997bed70a8dddcebeafbc090d6ed78700
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-03 10:56:45 +0500

    fixed category Repo

--- Solution1/IndexMarket.Infrastructure/Repository/CategoryRepositories/CategoryRepository.cs
+++ Solution1/IndexMarket.Infrastructure/Repository/CategoryRepositories/CategoryRepository.cs
@@ -1,11 +1,24 @@
 ﻿using IndexMarket.Domain.Entities;
 using IndexMarket.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace IndexMarket.Infrastructure.Repository;
 public class CategoryRepository 
     : BaseRepository<Category, Guid>, ICategoryRepository
 {
+    private readonly AppDbContext appDbContext;
     public CategoryRepository(AppDbContext appDbContext) 
         : base(appDbContext)
-    { }
+    {
+        this.appDbContext = appDbContext;
+    }
+
+    public async ValueTask<Category> GetCategoryByNameAsync(string categoryName)
+    {
+        var category = await this.appDbContext
+            .Set<Category>()
+            .FirstOrDefaultAsync(x => x.Title == categoryName);
+
+        return category;
+    }
 }
