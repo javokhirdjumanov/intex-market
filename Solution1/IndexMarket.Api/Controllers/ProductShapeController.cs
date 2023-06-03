8554f543ca6cb10fa6c5469c0dbadb47946898a9
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-04 01:54:07 +0500

    add new and point

---
+++ Solution1/IndexMarket.Api/Controllers/ProductShapeController.cs
@@ -0,0 +1,24 @@
+﻿using IndexMarket.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IndexMarket.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductShapeController : ControllerBase
+{
+    private readonly IProductShapeService productShapeService;
+
+    public ProductShapeController(IProductShapeService productShapeService)
+    {
+        this.productShapeService = productShapeService;
+    }
+
+    [HttpGet("All")]
+    public IActionResult GetProductShapes()
+    {
+        var productShapes = productShapeService.RetrieveProductsShapes();
+
+        return Ok(productShapes);
+    }
+}
