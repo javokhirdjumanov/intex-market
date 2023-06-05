46bf01d7096539269e8b3fe7651e42b8049222d7
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-05 17:14:51 +0500

    new Malumotlar papka added

---
+++ Solution1/IndexMarket.Api/AdditionalInformation/Scripts/0001_InsertSiteData.sql
@@ -0,0 +1,20 @@
+﻿
+
+INSERT INTO [dbo].[Sites] 
+    ([Id],
+    [PhoneNumber],
+    [JobTime],
+    [TelegrammLink],
+    [InstagramLink],
+    [Address_Id],
+    [CreatedAt],
+    [UpdatedAt])
+VALUES 
+   ('8865873b-b03e-428c-3f1a-08db62811147',
+    '998912345678',
+    '9:00 AM - 7:00 PM',
+    'https://t.me/example',
+    'https://www.instagram.com/example',
+    '8865873b-b03e-428c-3f1a-08db62811141',
+    GETDATE(),
+    GETDATE());
