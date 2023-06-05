0d69f956e699da39297e2509de0673a636736086
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-05 11:52:31 +0500

    add

---
+++ Solution1/IndexMarket.Infrastructure/Repository/ConsultationsRepositories/ConsultationRepository.cs
@@ -0,0 +1,11 @@
+﻿using IndexMarket.Domain.Entities;
+using IndexMarket.Infrastructure.Context;
+
+namespace IndexMarket.Infrastructure.Repository;
+public sealed class ConsultationRepository 
+    : BaseRepository<Consultation, Guid>, IConsultationRepository
+{
+    public ConsultationRepository(AppDbContext appDbContext) 
+        : base(appDbContext)
+    { }
+}
---
+++ Solution1/IndexMarket.Infrastructure/Repository/ConsultationsRepositories/IConsultationRepository.cs
@@ -0,0 +1,7 @@
+﻿using IndexMarket.Domain.Entities;
+
+namespace IndexMarket.Infrastructure.Repository;
+public interface IConsultationRepository 
+    : IBaseRepository<Consultation, Guid>
+{
+}
