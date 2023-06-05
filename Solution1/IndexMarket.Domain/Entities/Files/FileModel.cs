d5738cc8605db953a92c53353acabec40ca329dc
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-05 17:15:58 +0500

    add new files

---
+++ Solution1/IndexMarket.Domain/Entities/Files/FileModel.cs
@@ -0,0 +1,8 @@
+﻿namespace IndexMarket.Domain.Entities;
+
+public class FileModel
+{
+    public Guid Id { get; set; }
+    public string Type { get; set; }
+    public string FileName { get; set; }
+}
