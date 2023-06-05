7b0bf626cae0f23d5358957f8cca52cceca6fa60
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-05 10:38:58 +0500

    order repo fixed

--- Solution1/IndexMarket.Infrastructure/Repository/OrdersRepositories/OrderRepository.cs
+++ Solution1/IndexMarket.Infrastructure/Repository/OrdersRepositories/OrderRepository.cs
@@ -1,11 +1,15 @@
 ﻿using IndexMarket.Domain.Entities;
 using IndexMarket.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace IndexMarket.Infrastructure.Repository;
 public class OrderRepository 
     : BaseRepository<Order, Guid>, IOrderRepository
 {
+    private readonly AppDbContext appDbContext;
     public OrderRepository(AppDbContext appDbContext) 
         : base(appDbContext)
-    { }
+    {
+        this.appDbContext = appDbContext;
+    }
 }
