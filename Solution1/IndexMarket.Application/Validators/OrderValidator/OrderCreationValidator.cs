d1b5b86aa4b836d5fe11b40aee139e0566d3fc2e
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-05 09:50:05 +0500

    add order vaidation

---
+++ Solution1/IndexMarket.Application/Validators/OrderValidator/OrderCreationValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using IndexMarket.Application.DataTransferObject;
+
+namespace IndexMarket.Application.Validators;
+public class OrderCreationValidator : AbstractValidator<OrderCreationDto>
+{
+    public OrderCreationValidator()
+    {
+        RuleFor(o => o)
+            .NotNull().WithMessage("Object cannot null !");
+
+        RuleFor(o => o.Product_Id)
+            .NotEqual(default(Guid))
+            .WithMessage("Product Id cannot default guid !");
+
+        RuleFor(o => o.User_Id)
+            .NotEqual(default(Guid))
+            .WithMessage("User Id cannot default guid !");
+
+        RuleFor(o => o.Address_Id)
+            .NotEqual(default(Guid))
+            .WithMessage("Address Id cannot default guid !");
+    }
+}
