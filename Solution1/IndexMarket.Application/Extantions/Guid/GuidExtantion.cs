cf1a015658da2c23a890f6eb2f5e0dae68a94019
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-06 11:49:41 +0500

    add

---
+++ Solution1/IndexMarket.Application/Extantions/Guid/GuidExtantion.cs
@@ -0,0 +1,14 @@
+﻿using IndexMarket.Domain.Entities;
+using IndexMarket.Domain.Exceptions;
+
+namespace IndexMarket.Application.Extantions;
+public static class GuidExtantion
+{
+    public static void IsDefault(this Guid guid)
+    {
+        if(guid.Equals(default(Guid)))
+        {
+            throw new ValidationException($"The given Id is invalid: {guid}");
+        }
+    }
+}
