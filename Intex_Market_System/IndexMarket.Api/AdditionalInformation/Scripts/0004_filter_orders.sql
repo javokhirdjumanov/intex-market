a064963df139985a2f302ecaa97177925eb6677d
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-08 15:02:41 +0500

    changes scripts

---
+++ Intex_Market_System/IndexMarket.Api/AdditionalInformation/Scripts/0004_filter_orders.sql
@@ -0,0 +1,9 @@
+﻿
+SELECT *
+    FROM Orders AS o
+        INNER JOIN Users AS u ON o.UserId = u.Id
+        INNER JOIN Products AS p ON o.ProductId = p.Id
+        INNER JOIN ProductShapes AS ps ON p.ProductShapeId = ps.Id
+    WHERE LOWER(u.FirstName) LIKE '%' | LOWER(columnName) | '%' OR
+          u.PhoneNumber LIKE '%' | columnName | '%' OR
+          LOWER(ps.Name) LIKE '%' | LOWER(columnName) | '%';
