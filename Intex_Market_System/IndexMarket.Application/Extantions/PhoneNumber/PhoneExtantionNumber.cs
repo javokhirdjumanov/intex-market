41373cf9f9484d00537c47f02043a899bf6f0d6f
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-08 14:26:18 +0500

    add new extantion method

---
+++ Intex_Market_System/IndexMarket.Application/Extantions/PhoneNumber/PhoneExtantionNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace IndexMarket.Application.Extantions;
+public static class PhoneExtantionNumber
+{
+    public static string PhoneNumberCleaner(this string phoneNumber)
+    {
+        if (phoneNumber == null)
+        {
+            return phoneNumber;
+        }
+
+        phoneNumber = phoneNumber.Replace("+", string.Empty);
+        phoneNumber = phoneNumber.Replace("-", string.Empty);
+        phoneNumber = phoneNumber.Replace(" ", string.Empty);
+        phoneNumber = phoneNumber.Replace("(", string.Empty);
+        phoneNumber = phoneNumber.Replace(")", string.Empty);
+
+        return phoneNumber;
+    }
+}
