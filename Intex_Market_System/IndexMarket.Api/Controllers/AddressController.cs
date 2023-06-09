f2cd7dc8e9f2e79772ce3d1a6773a71d928052c7
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-09 11:38:13 +0500

    add

---
+++ Intex_Market_System/IndexMarket.Api/Controllers/AddressController.cs
@@ -0,0 +1,22 @@
+﻿using IndexMarket.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IndexMarket.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AddressController : ControllerBase
+{
+    private readonly IAddressService _addressService;
+    public AddressController(IAddressService addressService)
+    {
+        _addressService = addressService;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllAddress()
+    {
+        var address = _addressService.GetAddresses();
+
+        return Ok(address);
+    }
+}
