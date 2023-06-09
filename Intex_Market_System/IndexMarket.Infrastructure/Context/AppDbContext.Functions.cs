d3cedaa61fa2d8ef8e3d01f9b1753a5e668b30f6
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-09 15:13:58 +0500

    add

--- Intex_Market_System/IndexMarket.Infrastructure/Context/AppDbContext.Functions.cs
+++ Intex_Market_System/IndexMarket.Infrastructure/Context/AppDbContext.Functions.cs
@@ -1,5 +1,6 @@
 ﻿using IndexMarket.Domain.Entities;
 using IndexMarket.Infrastructure.Repository;
+using IndexMarket.Infrastructure.Repository.OrdersRepositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace IndexMarket.Infrastructure.Context;
@@ -8,4 +9,9 @@ public partial class AppDbContext
     [DbFunction("get_adresses", Schema = "public")]
     public IQueryable<get_address_model> GetAllAddress()
         => FromExpression(() => GetAllAddress());
+
+    [DbFunction("filter_products_price_in_order", Schema = "public")]
+    public IQueryable<filter_products_price_in_order_model>
+        Filter_Orders_By_Product_Price(decimal? from_price, decimal? to_price)
+        => FromExpression(() => Filter_Orders_By_Product_Price(from_price, to_price));
 }
