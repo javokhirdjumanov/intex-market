b5fd549584d40bc6bc37cbab0c5e6cde30074e0c
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-09 11:38:27 +0500

    add new function script

---
+++ Intex_Market_System/IndexMarket.Api/AdditionalInformation/Scripts/0005_get_address_function.sql
@@ -0,0 +1,23 @@
+﻿create or replace function get_adresses ()
+returns table (
+		iid uuid,
+		country character varying,
+		city character varying,
+		region character varying,
+		street character varying,
+		postal_code smallint) 
+language plpgsql
+as $$
+begin
+	return query 
+		select adress."Id" as iid,
+			   adress."Country" as country,
+			   adress."City" as city,
+			   adress."Region" as region,
+			   adress."Street" as street,
+			   adress."PostalCode" as postal_code
+		from public."Addresses" as adress;
+end;$$
+
+SELECT * FROM public.get_adresses ();
+
